17d48adceb0342f7eca86873444bca2dc1ab02fd
Muhammad Dafha Syahrizal <143611789+zalb0y@users.noreply.github.com>
2026-02-24 11:21:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
     // Ganti dengan URL dari hasil Deploy Google Apps Script Anda nanti
-    APP_SCRIPT_URL: "MASUKKAN_URL_WEB_APP_APPS_SCRIPT_DI_SINI" 
+    APP_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyNicY2G-ZS_lqQ9gMh2XKoOU5JwpMoZkS0J9XThj5FnD85zLx2jfCB3Oqcaj4Q-G4-JQ/exec" 
+
 };
